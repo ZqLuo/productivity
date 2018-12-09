14fc1726830fa03fd62dbb84eddd57393d2eba26
ahren <540464544@qq.com>
2018-12-09 19:58:51 +0800

    AES工具类修改

--- util/src/main/java/com/productivity/util/AESPasswordUtil.java
+++ util/src/main/java/com/productivity/util/AESPasswordUtil.java
@@ -1,179 +1,92 @@
 package com.productivity.util;
 
-import javax.crypto.*;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.UnsupportedEncodingException;
-import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
 public class AESPasswordUtil {
+    private static final String KEY_ALGORITHM = "AES";
+    private static final String DEFAULT_CIPHER_ALGORITHM = "AES/ECB/PKCS5Padding";//默认的加密算法
+
     /**
-     * 加密
+     * AES 加密操作
      *
-     * @param content
-     *            需要加密的内容
-     * @param password
-     *            加密密码
-     * @return
+     * @param content 待加密内容
+     * @param password 加密密码
+     * @return 返回Base64转码后的加密数据
      */
-    public static byte[] encrypt(String content, String password) {
+    public static String encrypt(String content, String password) {
         try {
-//            KeyGenerator kgen = KeyGenerator.getInstance("AES");
-//            kgen.init(128, new SecureRandom(password.getBytes()));
-            KeyGenerator kgen = KeyGenerator.getInstance("AES");
-            SecureRandom random=SecureRandom.getInstance("SHA1PRNG");
-            random.setSeed(password.getBytes());
-            kgen.init(128, random);
-            SecretKey secretKey = kgen.generateKey();
-            byte[] enCodeFormat = secretKey.getEncoded();
-            SecretKeySpec key = new SecretKeySpec(enCodeFormat, "AES");
-            Cipher cipher = Cipher.getInstance("AES");// 创建密码器
+            Cipher cipher = Cipher.getInstance(DEFAULT_CIPHER_ALGORITHM);// 创建密码器
+
             byte[] byteContent = content.getBytes("utf-8");
-            cipher.init(Cipher.ENCRYPT_MODE, key);// 初始化
-            byte[] result = cipher.doFinal(byteContent);
-            return result; // 加密
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (NoSuchPaddingException e) {
-            e.printStackTrace();
-        } catch (InvalidKeyException e) {
-            e.printStackTrace();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (IllegalBlockSizeException e) {
-            e.printStackTrace();
-        } catch (BadPaddingException e) {
-            e.printStackTrace();
+
+            cipher.init(Cipher.ENCRYPT_MODE, getSecretKey(password));// 初始化为加密模式的密码器
+
+            byte[] result = cipher.doFinal(byteContent);// 加密
+
+            return Base64Utils.encode(result);//通过Base64转码返回
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
+
         return null;
     }
 
     /**
-     * 密码加密，直接返回加密后字符串
-     * @param password
-     * @param salt
-     * @return
-     */
-    public static String encryptPassword(String password,String salt){
-        return parseByte2HexStr(encrypt(password,salt));
-    }
-
-    /**
-     * 解密
+     * AES 解密操作
      *
      * @param content
-     *            待解密内容
      * @param password
-     *            解密密钥
      * @return
      */
-    public static byte[] decrypt(byte[] content, String password) {
+    public static String decrypt(String content, String password) {
+
         try {
-//            KeyGenerator kgen = KeyGenerator.getInstance("AES");
-//            kgen.init(128, new SecureRandom(password.getBytes()));
-            KeyGenerator kgen = KeyGenerator.getInstance("AES");
-            SecureRandom random=SecureRandom.getInstance("SHA1PRNG");
-            random.setSeed(password.getBytes());
-            kgen.init(128, random);
-            SecretKey secretKey = kgen.generateKey();
-            byte[] enCodeFormat = secretKey.getEncoded();
-            SecretKeySpec key = new SecretKeySpec(enCodeFormat, "AES");
-            Cipher cipher = Cipher.getInstance("AES");// 创建密码器
-            cipher.init(Cipher.DECRYPT_MODE, key);// 初始化
-            byte[] result = cipher.doFinal(content);
-            return result; // 加密
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (NoSuchPaddingException e) {
-            e.printStackTrace();
-        } catch (InvalidKeyException e) {
-            e.printStackTrace();
-        } catch (IllegalBlockSizeException e) {
-            e.printStackTrace();
-        } catch (BadPaddingException e) {
-            e.printStackTrace();
+            //实例化
+            Cipher cipher = Cipher.getInstance(DEFAULT_CIPHER_ALGORITHM);
+
+            //使用密钥初始化，设置为解密模式
+            cipher.init(Cipher.DECRYPT_MODE, getSecretKey(password));
+
+            //执行操作
+            byte[] result = cipher.doFinal(Base64Utils.decode(content));
+
+            return new String(result, "utf-8");
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
+
         return null;
     }
 
-    public static String decryptPassword(String password,String salt){
-        byte[] decode = parseHexStr2Byte(password);
+    /**
+     * 生成加密秘钥
+     *
+     * @return
+     */
+    private static SecretKeySpec getSecretKey(final String salt) throws Exception {
+        //返回生成指定算法密钥生成器的 KeyGenerator 对象
+        KeyGenerator kg = null;
+        SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
+        secureRandom.setSeed(salt.getBytes());
         try {
-            return new String(decrypt(decode,salt), "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        return "";
-    }
+            kg = KeyGenerator.getInstance(KEY_ALGORITHM);
 
-    /**
-     * 将二进制转换成16进制
-     *
-     * @param buf
-     * @return
-     */
-    public static String parseByte2HexStr(byte buf[]) {
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < buf.length; i++) {
-            String hex = Integer.toHexString(buf[i] & 0xFF);
-            if (hex.length() == 1) {
-                hex = '0' + hex;
-            }
-            sb.append(hex.toUpperCase());
-        }
-        return sb.toString();
-    }
+            //AES 要求密钥长度为 128
+            kg.init(128, secureRandom);
 
-    /**
-     * 将16进制转换为二进制
-     *
-     * @param hexStr
-     * @return
-     */
-    public static byte[] parseHexStr2Byte(String hexStr) {
-        if (hexStr.length() < 1)
-            return null;
-        byte[] result = new byte[hexStr.length() / 2];
-        for (int i = 0; i < hexStr.length() / 2; i++) {
-            int high = Integer.parseInt(hexStr.substring(i * 2, i * 2 + 1), 16);
-            int low = Integer.parseInt(hexStr.substring(i * 2 + 1, i * 2 + 2), 16);
-            result[i] = (byte) (high * 16 + low);
-        }
-        return result;
-    }
+            //生成一个密钥
+            SecretKey secretKey = kg.generateKey();
 
-    /**
-     * 加密
-     *
-     * @param content
-     *            需要加密的内容
-     * @param password
-     *            加密密码
-     * @return
-     */
-    public static byte[] encrypt2(String content, String password) {
-        try {
-            SecretKeySpec key = new SecretKeySpec(password.getBytes(), "AES");
-            Cipher cipher = Cipher.getInstance("AES/ECB/NoPadding");
-            byte[] byteContent = content.getBytes("utf-8");
-            cipher.init(Cipher.ENCRYPT_MODE, key);// 初始化
-            byte[] result = cipher.doFinal(byteContent);
-            return result; // 加密
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (NoSuchPaddingException e) {
-            e.printStackTrace();
-        } catch (InvalidKeyException e) {
-            e.printStackTrace();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (IllegalBlockSizeException e) {
-            e.printStackTrace();
-        } catch (BadPaddingException e) {
-            e.printStackTrace();
+            return new SecretKeySpec(secretKey.getEncoded(), KEY_ALGORITHM);// 转换为AES专用密钥
+        } catch (NoSuchAlgorithmException ex) {
+            ex.printStackTrace();
         }
+
         return null;
     }
-
 }
