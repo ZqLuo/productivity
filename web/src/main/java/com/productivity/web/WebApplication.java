c07d3babcec99b52c416cffd0c06174429be343b
ahren <540464544@qq.com>
2019-01-05 14:39:27 +0800

    去除空引用

--- web/src/main/java/com/productivity/web/WebApplication.java
+++ web/src/main/java/com/productivity/web/WebApplication.java
@@ -1,6 +1,5 @@
 package com.productivity.web;
 
-import com.mysql.cj.jdbc.MysqlDataSourceFactory;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
